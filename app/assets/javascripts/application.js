17cbe7140d160000da8d1f9ba805656362fb2b1c
Calvin <mrcalvinkc@gmail.com>
2017-06-18 11:12:15 -0700

    Adding Jquery for Get Data Button

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,8 +20,35 @@ function changeColors(){
       document.getElementById('first').style.textShadow = "5px 5px 1px #ff0000, 10px 10px 1px #0000ff";
       setTimeout(function(){  
         document.getElementById('second').style.color = 'blue';
-        setTimeout(function(){document.getElementById('third').style.textShadow = "5px 5px 1px #ff0000, 10px 10px 1px #0000ff";
+        setTimeout(function(){
+          document.getElementById('third').style.textShadow = "5px 5px 1px #ff0000, 10px 10px 1px #0000ff";
+          setTimeout(function(){
+          document.getElementById('fourth').style.textShadow = "5px 5px 1px #ff0000, 10px 10px 1px #0000ff";
+          }, 1000); 
         }, 1000);
       }, 1000);
     }, 1000);
 }
+// JS Equivalent: Jquery
+
+function getData(){
+  var registeredBizDiv = document.getElementById('registeredbusiness');
+
+  $.get('https://data.sfgov.org/resource/vbiu-2p9h.json', function(response){
+      console.log(response); // Make sures it doesnt just bypass if something goes wrong. - all because JS is asynchronous 
+      var htmlString = '';
+      for (var i = 0; i < response.length; i++) {
+        var business = response[i];
+        htmlString += '<div>';
+        htmlString += '<h1>' + business.dba_name + '</h1>';
+        htmlString += '<h4>' + business.city + '</h1>';
+        htmlString += '<h4>' + business.naic_code_description + '</h1>';
+        htmlString += '<h4>' + business.ttxid + '</h1>';
+        htmlString += '<h4>' + business.dba_name + '</h1>';
+        htmlString += '</div>';
+      }
+      registeredBizDiv.innerHTML = htmlString;
+    }
+  );
+}
+
